ece740512a8743f8a2fdd75133ceee0e2e0f153a
Joaquim Perez <jquim1@hotmail.com>
2018-04-10 10:59:14 +0100

    Cleanup

--- index.js
+++ index.js
@@ -14,33 +14,34 @@ const types =
 	}
 ];
 
+// Default values
 const DEFAULT_TYPE = 0;
-const DEFAULT_WIDTH = 500;
-const DEFAULT_HEIGHT = 250;
+const DEFAULT_IMAGE_WIDTH = 500;
+const DEFAULT_IMAGE_HEIGHT = 250;
+
+// Parameters
+const IMAGE_WIDTH = "image_width";
+const IMAGE_HEIGHT = "image_height";
 
 $(document).ready(function()
 {
 	var currentURL = purl(window.location.href);
 	var params = currentURL.param();
 
-	if(isNaN(params["w"])) params["w"] = String(DEFAULT_WIDTH);
-	if(isNaN(params["h"]))  params["h"] = String(DEFAULT_HEIGHT);
+	if(isNaN(params[IMAGE_WIDTH])) params[IMAGE_WIDTH] 	= String(DEFAULT_IMAGE_WIDTH);
+	if(isNaN(params[IMAGE_HEIGHT]))  params[IMAGE_HEIGHT] = String(DEFAULT_IMAGE_HEIGHT);
 
 	/* Dropdown */
 	types.forEach(function(type)
 	{
 		$(".dropdown-menu").append("<li><a href='#'>"+type["label"]+"</a></li>");
-	});				
-      $(".dropdown-menu").append('</ul>');
+	});
 
 	/* Background image */
-	$(".img-container").append('<img src="" class="img" width="'+params["w"]+'" height="'+params["h"]+'">');
-	$(".img-container").append('<div class="img-overlay" id="overlay">');
-		$(".img-overlay").css('top',params["h"]*0.09+"%");
-		$(".img-overlay").css('left',params["w"]*0.05+"%");
-		$(".img-overlay").append('<textarea class="form-control" id="msg1">');
-		$(".img-overlay").append('<textarea class="form-control" id="msg2">');		
-				
+	$(".img").attr('width',params[IMAGE_WIDTH]);
+	$(".img").attr('height',params[IMAGE_HEIGHT]);
+	$(".img-overlay").css('top',params[IMAGE_HEIGHT]*0.09+"%");
+	$(".img-overlay").css('left',params[IMAGE_WIDTH]*0.05+"%");
 	setImage(DEFAULT_TYPE);
 
 	/* Dropdown action */
@@ -55,8 +56,8 @@ $(document).ready(function()
 	{
 	    html2canvas($(".img-container"), 
 	    {
-	    	  width: params["w"],
-	    	  height: params["h"],
+	    	  width: params[IMAGE_WIDTH],
+	    	  height: params[IMAGE_HEIGHT],
 	        onrendered: function(canvas) 
 	        {
 	            var image = canvas.toDataURL("image/png");
